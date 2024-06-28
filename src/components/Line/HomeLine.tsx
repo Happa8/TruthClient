2495a2587125e6a7797035dd5e53e3512798c41b
Happa8 <happa.eight@gmail.com>
2024-06-29 00:22:25 +0900

    scroll fix [WIP]

--- src/components/Line/HomeLine.tsx
+++ src/components/Line/HomeLine.tsx
@@ -17,25 +17,31 @@ const HomeLine: FC = () => {
   const TopMarkerRef = useRef<HTMLDivElement>(null);
   const TopMarkerViewPosition = useOnScreen(TopMarkerRef);
 
+  // 以前のTruth表示部の高さを保持
   const prevHeightRef = useRef(0);
 
+  // スクロール固定
   useEffect(() => {
+    // Refが存在しない場合は何もしない
     if (!ListRef.current) return;
+
+    // 以前の高さが0の場合は初期化
     if (prevHeightRef.current === 0) {
       prevHeightRef.current = ListRef.current.scrollSize;
       return;
     }
 
+    // 一番上が表示されている場合は何もしない
     if (TopMarkerViewPosition === "VISIBLE") {
       prevHeightRef.current = ListRef.current.scrollSize;
       return;
     }
 
+    // 以前の高さと現在の高さを比較して、増えた文だけ現在のスクロール位置を巻き戻す
     const currentHeight = ListRef.current.scrollSize;
     const currentPos = ListRef.current.scrollOffset;
-    ListRef.current.scrollTo(
-      currentPos + currentHeight - prevHeightRef.current
-    );
+    // TODO:なんかうまくいかない
+    // ListRef.current.scrollBy(-(currentHeight - prevHeightRef.current));
 
     prevHeightRef.current = currentHeight;
   }, [postList.length]);
