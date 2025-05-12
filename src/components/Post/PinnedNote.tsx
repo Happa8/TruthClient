d31d6115c45d13fe24a0239a719d314e065981b8
Happa8 <happa.eight@gmail.com>
2025-05-12 23:08:57 +0900

    不必要なimport削除

--- src/components/Post/PinnedNote.tsx
+++ src/components/Post/PinnedNote.tsx
@@ -1,6 +1,6 @@
 import { css } from "@/styled-system/css";
-import { FC, ReactNode } from "react";
-import { MdPin, MdPushPin, MdRepeat } from "react-icons/md";
+import { FC } from "react";
+import { MdPushPin } from "react-icons/md";
 
 type Props = {
   // children: ReactNode;
